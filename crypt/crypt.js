38c29c5679f81ba33e48de4f33664e43b84aba2d
plus <plu5@users.noreply.github.com>
2026-04-22 12:37:37 +0300

    feat(crypt): Clear button

--- crypt/crypt.js
+++ crypt/crypt.js
@@ -1,3 +1,17 @@
+const domkey = document.getElementById("key");
+const domerr = document.getElementById("err");
+const dominput = document.getElementById("input");
+const domoutput = document.getElementById("output");
+const inputs = [domkey, dominput, domoutput];
+const clearbtn = document.getElementById("clear");
+const encryptbtn = document.getElementById("encrypt");
+
+const clear = () => {
+  for (let inp of inputs) {
+    inp.value = "";
+  }
+};
+
 const encode = (text) => new TextEncoder().encode(text);
 
 const genCounter = () => window.crypto.getRandomValues(new Uint8Array(16));
@@ -8,21 +22,19 @@ const genKey_ = async () => window.crypto.subtle.generateKey(
 const genKey = async () => {
   const key = await genKey_();
   const exported = await window.crypto.subtle.exportKey("jwk", key);
-  document.getElementById("key").value = exported.k;
+  domkey.value = exported.k;
   return key;
 };
 
 const getKey = async () => {
   let key = null;
-  const element = document.getElementById("key");
-  if (element.value.length > 0) {
+  if (domkey.value.length > 0) {
     try {
       key = await window.crypto.subtle.importKey(
-        "jwk", {k: element.value, kty: "oct"}, "AES-CTR", true,
+        "jwk", {k: domkey.value, kty: "oct"}, "AES-CTR", true,
         ["encrypt", "decrypt"]);
     } catch (e) {
-      document.getElementById("err").
-        textContent = `Unable to import key. ${e}`;
+      domerr.textContent = `Unable to import key. ${e}`;
     }
   } else {
     key = await genKey();
@@ -37,10 +49,11 @@ const arrayBufferToBase64 = (buffer) => btoa(
   String.fromCharCode(...new Uint8Array(buffer)));
 
 const encrypt = async () => {
-  let r = encode(document.getElementById("input").value);
+  let r = encode(dominput.value);
   getKey().then((key) => encrypt_(key, r)).then((a) => {
-    document.getElementById("output").value = arrayBufferToBase64(a);
+    domoutput.value = arrayBufferToBase64(a);
   });
 };
 
-document.getElementById("encrypt").addEventListener("click", encrypt);
+encryptbtn.addEventListener("click", encrypt);
+clearbtn.addEventListener("click", clear);
