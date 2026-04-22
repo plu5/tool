c2463fa4f28136b0e1af911621c9484413f6c0e5
plus <plu5@users.noreply.github.com>
2026-04-22 12:25:40 +0300

    feat(crypt): Import and export key

--- crypt/crypt.js
+++ crypt/crypt.js
@@ -2,8 +2,33 @@ const encode = (text) => new TextEncoder().encode(text);
 
 const genCounter = () => window.crypto.getRandomValues(new Uint8Array(16));
 
-const genKey = () => window.crypto.subtle.generateKey(
-  {name: "AES-CTR", length: 256}, true, ["encrypt", "decrypt"])
+const genKey_ = async () => window.crypto.subtle.generateKey(
+  {name: "AES-CTR", length: 256}, true, ["encrypt", "decrypt"]);
+
+const genKey = async () => {
+  const key = await genKey_();
+  const exported = await window.crypto.subtle.exportKey("jwk", key);
+  document.getElementById("key").value = exported.k;
+  return key;
+};
+
+const getKey = async () => {
+  let key = null;
+  const element = document.getElementById("key");
+  if (element.value.length > 0) {
+    try {
+      key = await window.crypto.subtle.importKey(
+        "jwk", {k: element.value, kty: "oct"}, "AES-CTR", true,
+        ["encrypt", "decrypt"]);
+    } catch (e) {
+      document.getElementById("err").
+        textContent = `Unable to import key. ${e}`;
+    }
+  } else {
+    key = await genKey();
+  }
+  return key;
+};
 
 const encrypt_ = (key, text) => window.crypto.subtle.encrypt(
   {name: "AES-CTR", counter: genCounter(), length: 64}, key, text);
@@ -11,10 +36,10 @@ const encrypt_ = (key, text) => window.crypto.subtle.encrypt(
 const arrayBufferToBase64 = (buffer) => btoa(
   String.fromCharCode(...new Uint8Array(buffer)));
 
-const encrypt = () => {
+const encrypt = async () => {
   let r = encode(document.getElementById("input").value);
-  genKey().then((key) => encrypt_(key, r)).then((a) => {
-    document.getElementById("output").textContent = arrayBufferToBase64(a);
+  getKey().then((key) => encrypt_(key, r)).then((a) => {
+    document.getElementById("output").value = arrayBufferToBase64(a);
   });
 };
 
