d27e8ac732679c56a078d3e11890e2f6aef9dbe0
plus <plu5@users.noreply.github.com>
2026-04-23 08:59:57 +0300

    feat(crypt): Functional decryption
    There were two reasons decryption didn't work with my previous code,
    both we need the same counter/iv, and second it was wrong to do
    `encode(dominput.value)` because it's base64 not plain text.
    
    I split the UI for encryption and decryption (mostly because I didn't
    want the IV to be an input for encryption, which I think would have
    encouraged reusing the same IV with the same key, which is exactly
    what you need to avoid with AES-CTR) and added a button to transfer
    down values for usability.
    
    Also moved err (element where errors are shown) to the top for
    visibility.

--- crypt/crypt.js
+++ crypt/crypt.js
@@ -1,17 +1,29 @@
+const e = (id) => document.getElementById(id);
+
 const domkey = document.getElementById("key");
 const domerr = document.getElementById("err");
-const dominput = document.getElementById("input");
-const domoutput = document.getElementById("output");
-const inputs = [domkey, dominput, domoutput];
+const domencrypt = {
+  btn: e("encrypt"), in: e("in"), out: e("out"), iv: e("iv"),
+};
+const domdecrypt = {
+  btn: e("decrypt"), in: e("in2"), out: e("out2"), iv: e("iv2"),
+  transfer: e("transfer"),
+};
+const inputs = [domkey,
+                domencrypt.in, domencrypt.out, domencrypt.iv,
+                domdecrypt.in, domdecrypt.out, domdecrypt.iv];
 const clearbtn = document.getElementById("clear");
-const encryptbtn = document.getElementById("encrypt");
-const decryptbtn = document.getElementById("decrypt");
 
 const clear = () => {
   for (let inp of inputs) {
     inp.value = "";
   }
 };
+
+const transfer = () => {
+  domdecrypt.in.value = domencrypt.out.value;
+  domdecrypt.iv.value = domencrypt.iv.value;
+}
 
 const encode = (text) => new TextEncoder().encode(text);
 const decode = (text) => new TextDecoder().decode(text);
@@ -44,24 +56,31 @@ const getKey = async () => {
   return key;
 };
 
-const encryptBuffer = (key, buffer) => window.crypto.subtle.encrypt(
-  {name: "AES-CTR", counter: genCounter(), length: 64}, key, buffer);
-const decryptBuffer = (key, buffer) => window.crypto.subtle.decrypt(
-  {name: "AES-CTR", counter: genCounter(), length: 64}, key, buffer);
+const encryptBuffer = (key, iv, buffer) => window.crypto.subtle.encrypt(
+  {name: "AES-CTR", counter: iv, length: 64}, key, buffer);
+const decryptBuffer = (key, iv, buffer) => window.crypto.subtle.decrypt(
+  {name: "AES-CTR", counter: iv, length: 64}, key, buffer);
 
 const arrayBufferToBase64 = (buffer) => btoa(
   String.fromCharCode(...new Uint8Array(buffer)));
+const base64ToArrayBuffer = (base64) => Uint8Array.from(
+  atob(base64), c => c.charCodeAt(0));
 
 const encrypt = async () => {
-  domoutput.value = arrayBufferToBase64(
-    await encryptBuffer(await getKey(), encode(dominput.value)));
+  const iv = genCounter();
+  domencrypt.iv.value = arrayBufferToBase64(iv);
+  domencrypt.out.value = arrayBufferToBase64(
+    await encryptBuffer(await getKey(), iv, encode(domencrypt.in.value)));
 };
 
 const decrypt = async () => {
-  domoutput.value = decode(
-    await decryptBuffer(await getKey(), encode(dominput.value)));
+  const iv = base64ToArrayBuffer(domdecrypt.iv.value);
+  domdecrypt.out.value = decode(
+    await decryptBuffer(await getKey(), iv,
+                        base64ToArrayBuffer(domdecrypt.in.value)));
 };
 
 clearbtn.addEventListener("click", clear);
-encryptbtn.addEventListener("click", encrypt);
-decryptbtn.addEventListener("click", decrypt);
+domencrypt.btn.addEventListener("click", encrypt);
+domdecrypt.btn.addEventListener("click", decrypt);
+domdecrypt.transfer.addEventListener("click", transfer);
