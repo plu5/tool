f59a09b4161be4d9cb6ac086144464a61985ad75
plus <plu5@users.noreply.github.com>
2026-06-28 04:58:42 +0300

    feat(date): Ability to modify input date

--- date/date.js
+++ date/date.js
@@ -1,15 +1,27 @@
+function e(id) {
+  return document.getElementById(id);
+}
+
 function pad2(num) {
   s = num.toString();
   return s.length == 2 ? s : '0' + s;
 }
 
-const now = new Date();
+function clear() {
+  e("input").value = "";
+}
 
-// ,2026-06-26 11:28:16,1782462496.5603962,2026-06-27 03:24:25,1782519865.7709942
-// (,creation,creation epoch,modification,modification epoch)
-// In our case making creation and modification all be current time
-const localeStr = `${now.getFullYear()}-${pad2(now.getMonth()+1)}-${pad2(now.getDate())} ${pad2(now.getHours())}:${pad2(now.getMinutes())}:${pad2(now.getSeconds())}`
-const epochStr = `${now.getTime()/1000}`
-const template = `,${localeStr},${epochStr},${localeStr},${epochStr}`;
+function set() {
+  const s = e('input').value;
+  const now = s ? new Date(s) : new Date();
+  const localeStr = `${now.getFullYear()}-${pad2(now.getMonth()+1)}-${pad2(now.getDate())} ${pad2(now.getHours())}:${pad2(now.getMinutes())}:${pad2(now.getSeconds())}`
+  const epochStr = `${now.getTime()/1000}`
+  const template = `${localeStr},${epochStr}`;
+  e("output").value = template;
+  if (e("input").value == '') e("input").value = localeStr;
+}
 
-document.getElementById("output").textContent = template;
+e("clear").addEventListener("click", clear);
+e("set").addEventListener("click", set);
+
+set()
